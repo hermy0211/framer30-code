f56bd9eea807180388a237ce3edc01b7207bc698
Anne Lee <hermy0211@naver.com>
2018-10-28 16:43:30 +0900

    Updated Project 10

--- Day 10 Favorites/App.tsx
+++ Day 10 Favorites/App.tsx
@@ -1,49 +1,19 @@
 import { Data, animate, Override, Animatable } from "framer"
 
 const data = Data({
-    heartScale: [Animatable(1), Animatable(1), Animatable(1)]
+    frameTop: Animatable(0)
 })
 
-const springOptions = {
-    tension: 500,
-    friction: 14
-}
+let clicks = 1
 
-export const heartIcon: Override = () => {
+export const cardFrame: Override = props => {
     return {
-        scale: data.heartScale[0],
+        top: data.frameTop,
         onTap() {
-            animate.spring(data.heartScale[0], 1.2, springOptions)
             setTimeout(()=>{
-                animate.spring(data.heartScale[0], 1, springOptions)
-            }, 200)
-            setTimeout(()=>{
-                animate.easeOut(data.cardScale[0], 0, {duration: 0.2})
-            }, 600)
-        }
-    }
-}
-
-export const heartIconTwo: Override = () => {
-    return {
-        scale: data.heartScale[1],
-        onTap() {
-            animate.spring(data.heartScale[1], 1.2, springOptions)
-            setTimeout(()=>{
-                animate.spring(data.heartScale[1], 1, springOptions)
-            }, 100)
-        }
-    }
-}
-
-export const heartIconThree: Override = () => {
-    return {
-        scale: data.heartScale[2],
-        onTap() {
-            animate.spring(data.heartScale[2], 1.2, springOptions)
-            setTimeout(()=>{
-                animate.spring(data.heartScale[2], 1, springOptions)
-            }, 100)
+                animate.ease(data.frameTop, -120*clicks, {duration: 0.4})
+                clicks++
+            }, 400)
         }
     }
 }
